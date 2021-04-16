4798070fa8c17e53531e9ef39c21d823fd8a08be
unknown <619568942@qq.com>
2021-04-16 10:53:42 +0300

    part4 change

--- part4/bloglist/mongo.js
+++ part4/bloglist/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const mongoUrl = 'mongodb+srv://fullstack:mongodb@cluster0.xf3xg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const mongoUrl = 'mongodb+srv://fullstack:password@cluster0.xf3xg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const blogSchema = new mongoose.Schema({
